1b74f4704a582371ab7c7c43ed11e56c5994d3b8
zzt <zzt@qq.com>
2021-04-10 23:11:07 +0800

    11

---
+++ src/main/java/com/zzt/springboot/BootTextApplication.java
@@ -0,0 +1,14 @@
+package com.zzt.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class BootTextApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootTextApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zzt/springboot/BootTextApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zzt.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootTextApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
